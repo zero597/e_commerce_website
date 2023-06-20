727036be93e856e4bf56abc2260c2e8e10f69132
zero597 <443746099@qq.com>
2023-06-20 18:25:16 +0800

    4.1,修改gateway测试异常

---
+++ gateway-service/src/test/java/edu/gdkm/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gdkm;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
